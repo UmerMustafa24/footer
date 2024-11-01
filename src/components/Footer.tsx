e177484d0a9dc6e81487dfe2f0531cf2d68606ab
UmerMustafa24 <umermustafawork2010@gmail.com>
2024-11-01 23:27:34 +0500

    commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+    return (
+        <div>
+        <footer className="ml-5 mt-4">
+        
+            <h2 className="font-bold">Contact Us:</h2>
+            <p>Phone: 923467578</p>
+            <br />
+            <h2 className="font-bold">Email:</h2>
+            <p>xys@gmail.com</p>
+            <br />
+            <h2 className="font-bold">Visit Us At:</h2>
+            <p>ABC Road, food Street, Karachi</p>
+            <br />
+            <hr />
+            <br />
+            <h3>ALL RIGHTS RESERVED ®</h3>
+            </footer>
+            </div>
+        
+    );
+} 
